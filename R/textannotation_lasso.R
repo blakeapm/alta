6ec6ed863b76eab0ab4b4b05e74c9e36fc4c721c
Blake Miller <blakeapm@gmail.com>
2020-04-11 01:55:07 +0100

    add textannotation_random function

--- R/textannotation_lasso.R
+++ R/textannotation_lasso.R
@@ -2,27 +2,38 @@
 #'
 #' This function supports annotation of documents using an uncertainty sampling approach using predicted response of lasso regression as an uncertainty measure. This algorithm samples a batch of unlabeled documents with a predicted probability nearest to .5 for annotation via the R console. This function returns a data frame with annotations and their resepective batch_id and a boolean flag for whether or not the documents were sampled actively (or randomly).
 #' @param data A dataframe with a 'text' column and 'doc_id' column.
-#' @param text_col A string with the name of the column containing document text. Defaults to 'text'
-#' @param y_col A string with the name of the column containing document text. Defaults to 'y'
-#' @param doc_id_col A string with the name of the column containing document ids. Defaults to 'doc_id'
-#' @param batch_size An integer for the number of documents to sample at each iteration of the active learning algorithm. 
+#' @param text_col A string with the name of the column containing document 
+#' text. Defaults to 'text'
+#' @param y_col A string with the name of the column containing document 
+#' text. Defaults to 'y'
+#' @param doc_id_col A string with the name of the column containing document
+#'  ids. Defaults to 'doc_id'
+#' @param batch_size An integer for the number of documents to sample at 
+#' each iteration of the active learning algorithm. Defaults to 10.
 #' @param n_batches An integer for the number of total batches to label.
-#' @param rand_minority_min_n Boolean variable controlling whether new documents are selected via 'random' or 'active' sampling.
-#' @param active Boolean variable controlling whether new documents are selected via 'random' or 'active' sampling.
-#' @return An data frame object derived from the data frame specified in
-#' the 'data' parameter where the new annotations have been added to the
-#' the column specified in 'y_col', a new column 'batch' has been added
-#' to specify the batch at which each new document was labeled, and a new
-#' column 'active' specifying whether or not a newly-labeled document was
-#' sampled actively or passively (randomly).
+#' Defaults to 10..
+#' @param n_cores An integer for the number of cores to use in the estimation 
+#' of the lasso model.
+#' @param rand_minority_min_n An integer for the minimum number of documents
+#' in the minority class needed to begin active learning. If the minority
+#' class has fewer observations than `rand_minority_min_n`, documents will
+#' be sampled randomly. Defaults to 100.
+#' @return A new data frame object derived from the data frame passed into
+#' the the 'data' argument. This new data frame includes new annotations from
+#' the annotation session in the column specified in the 'y_col' argument. 
+#' A new column 'batch' has been added to specify the batch at which each 
+#' new document was labeled, and a new column 'active' has been added to 
+#' specify  whether a newly-labeled document was sampled actively or 
+#' passively (randomly).
+#' @import quanteda glmnet doMC
 #' @author Blake Miller
 #' @keywords active learning text supervised learning annotation
 #' @export
 #' @examples
-#' data <- read.csv('hate_speech_corpus.csv', stringsAsFactors=FALSE)
-#' data_annotated <- textannotation_lasso(data, text_col='comment_text', y_col='toxic', id_col='id', n_batches=100, batch_size=5)
+#' data <- read.csv('wikipedia_hate_speech.csv', stringsAsFactors=FALSE)
+#' data_annotated <- textannotation_lasso(data, text_col='comment_text', y_col='toxic', doc_id_col='id', n_batches=100, batch_size=5)
 
-textannotation_lasso <- function(data, text_col='text', y_col='y', doc_id_col='doc_id', n_batches=10, batch_size=10, rand_minority_min_n=100, active=TRUE) {
+textannotation_lasso <- function(data, text_col='text', y_col='y', doc_id_col='doc_id', n_batches=10, batch_size=10, rand_minority_min_n=100, n_cores=2) {
 	if (!y_col %in% names(data)) {
 		warning(paste("No existing y column found in corpus data frame. Creating new column: '", y_col, "\'", sep=""))
 		data[y_col] <- NA
@@ -47,7 +58,7 @@ textannotation_lasso <- function(data, text_col='text', y_col='y', doc_id_col='d
 		if (length(labeled) == nrow(data)) {
 			stop(paste("All documents are already labeled for the specified column: '", y_col, "\'", sep=""))
 		}
-		if (active == TRUE & nrow(labeled) > rand_minority_min_n - 1) {
+		if (nrow(labeled) > rand_minority_min_n - 1) {
 			is_active <- TRUE
 			x_l <- dfm_subset(dfm, !is.na(docvars(dfm, y_col)))
 			x_u <- dfm_subset(dfm, is.na(docvars(dfm, y_col)))
@@ -56,7 +67,7 @@ textannotation_lasso <- function(data, text_col='text', y_col='y', doc_id_col='d
 			} else {
 				message("\nRetraining lasso model...\n", sep="")
 			}
-			registerDoMC(cores=5)
+			registerDoMC(cores=n_cores)
 			mod <- cv.glmnet(x_l, docvars(x_l, y_col), family="binomial", alpha=1, nfolds=5, parallel=TRUE, intercept=TRUE)
 			pred <- predict(mod, x_u, s="lambda.1se", type="response")
 			#mod <- svm(y ~ x_l, x_u, cost = 10, scale = FALSE, kernel = 'linear')
@@ -66,14 +77,12 @@ textannotation_lasso <- function(data, text_col='text', y_col='y', doc_id_col='d
 			to_label$score <- sorted$x[1:batch_size]
 		} else {
 			is_active <- FALSE
-			if (active == TRUE) {
-				warning("Not enough labeled observations in the minority class. Randomly sampling...")
-			}
+			warning("Not enough labeled observations in the minority class. Randomly sampling...")
 			to_label <- unlabeled[sample(1:nrow(unlabeled), batch_size), ]
 		}
 		hrule <- "\n——————————————————————————————————————————————"
 		for (i in 1:nrow(to_label)) {
-			id <- to_label[i,id_col]
+			id <- to_label[i,doc_id_col]
 			text <- to_label[i,text_col]
 			score <- to_label[i,"score"]
 			message(paste(hrule, "\nDocument ID: ", id, "\n\n", "Score: ", score, "\n\n", "Document Text: ", "\n\n", text, hrule, sep=''))
@@ -85,9 +94,9 @@ textannotation_lasso <- function(data, text_col='text', y_col='y', doc_id_col='d
 					warning("Invalid input. You must enter either 0 for false or 1 for true.")
 				}
 			}
-			data[data[id_col] == id, y_col] <- label
-			data[data[id_col] == id, 'batch_id'] <- batch_id
-			data[data[id_col] == id, 'active'] <- is_active
+			data[data[doc_id_col] == id, y_col] <- label
+			data[data[doc_id_col] == id, 'batch_id'] <- batch_id
+			data[data[doc_id_col] == id, 'active'] <- is_active
 		}
 		message(paste(hrule, "\n\nDone with batch ", batch_id, "/", batch_size, ".\n\nRefitting model with annotations from the previous batch...\n\n", sep=""))
 	}
---
+++ R/textannotation_random.R
@@ -0,0 +1,48 @@
+#' Text Annotation Using Uncertainty Sampling via Lasso Regression
+#'
+#' This function supports annotation of documents using random sampling.
+#' @param data A dataframe with a 'text' column and 'doc_id' column.
+#' @param text_col A string with the name of the column containing document text. Defaults to 'text'
+#' @param y_col A string with the name of the column containing document text. Defaults to 'y'
+#' @param doc_id_col A string with the name of the column containing document ids. Defaults to 'doc_id'
+#' @param n_docs An integer for the number of documents to label in the session.
+#' @return An data frame object derived from the data frame specified in
+#' the 'data' parameter where the new annotations have been added to the
+#' the column specified in 'y_col', a new column 'active' has been added
+#' to specifying whether or not a newly-labeled document was sampled 
+#' actively or passively (randomly).
+#' @author Blake Miller
+#' @keywords active learning text supervised learning annotation
+#' @export
+#' @examples
+#' data <- read.csv('hate_speech.csv', stringsAsFactors=FALSE)
+#' data_annotated <- textannotation_random(data, text_col='comment_text', y_col='toxic', doc_id_col='id', n_docs=100)
+
+textannotation_random <- function(data, text_col='text', y_col='y', doc_id_col='doc_id', n_docs=100) {
+	if (!y_col %in% names(data)) {
+		warning(paste("No existing y column found in corpus data frame. Creating new column: '", y_col, "\'", sep=""))
+		data[y_col] <- NA
+	}
+	if (!'active' %in% names(data)) {
+		data$active <- NA
+	}
+	unlabeled <- data[which(is.na(data[y_col])),]
+	to_label <- unlabeled[sample(1:nrow(unlabeled), n_docs), ]
+	hrule <- "\n——————————————————————————————————————————————"
+	for (i in 1:nrow(to_label)) {
+		id <- to_label[i,doc_id_col]
+		text <- to_label[i,text_col]
+		message(paste(hrule, "\nDocument ID: ", id, "\n\n", "Document Text: ", "\n\n", text, hrule, sep=''))
+		while (TRUE) {
+			label <- readline(prompt=paste("\n\nEnter label for '", y_col, "' (0, 1): ", sep=""))
+			if (grepl("[0-1]", label)) {
+				break
+			} else {
+				warning("Invalid input. You must enter either 0 for false or 1 for true.")
+			}
+		}
+		data[data[doc_id_col] == id, y_col] <- label
+		data[data[doc_id_col] == id, 'active'] <- FALSE
+	}
+	data
+}
